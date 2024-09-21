2c8840eba9d3ad1825ab5710ed562fefc8924af4
GouthaM-Krish-nA <gouthamkp0@gmail.com>
2024-09-21 18:28:05 +0530

    fifth

--- exam1/q12.py
+++ exam1/q12.py
@@ -1,15 +1,15 @@
 n = int(input())
 l = list(map(int, input().split()))
-s = []
+p = []
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
             if (l[i] + l[j] + l[k]) == 0:
                 triplet = sorted([l[i], l[j], l[k]])  
-                if triplet not in s:  
-                    s.append(triplet)
+                if triplet not in k:  
+                    p.append(triplet)
 
-print(s)
+print(p)
 
               
 
