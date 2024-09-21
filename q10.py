5659daa458b75d2aadb06f0131f455bbd468a4fb
GouthaM-Krish-nA <gouthamkp0@gmail.com>
2024-09-21 18:33:31 +0530

    next

--- q10.py
+++
@@ -1,4 +0,0 @@
-n=int(input())
-l=list(map(int,input().split()))
-p=[x for x in range(l[0],max(l)) if x not in l]
-print(p)
--- q4.py
+++
@@ -1,8 +0,0 @@
-n=int(input())
-l=list(map(int,input().split()))
-sum=0
-p=[]
-for i in range(n):
-    sum=sum+l[i]
-    p.append(sum)
-print(p)    
